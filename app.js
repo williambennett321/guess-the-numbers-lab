d597f4bd4458914176294580f866cca2d0b3e10e
williambennett321 <william_bennett@DESKTOP-U4DJP4P.localdomain>
2021-10-10 13:37:08 -0400

    add render method and getGuess method to game

--- app.js
+++ app.js
@@ -3,27 +3,62 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
-  prevGuesses : [],
-  getGuess : function() {
-    this.playerGuess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`)
-    while (this.playerGuess === Number) 
-    {
-      if (this.playerGuess > 0 && this.playerGuess < 100) 
-      {
-        return this.playerGuess
-      } 
-      else 
-      {
-        return NaN
-      } 
-    }
+  prevGuesses: [],
+
+  getGuess: function () {
+
+    let playerGuess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`)
+
+    let parseNum = parseInt(playerGuess)
+
+    return parseNum
+
   },
-  play : function() {
-    this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-  }
+
+
+
+      
+      play: function() {
+        this.secretNum = Math.floor(Math.random() *
+          (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+      
+          let playerAns = this.getGuess()
+    
+      while(this.playerGuess !== this.secretNum) 
+      {
+        this.prevGuesses.push(this.playerAns)
+        
+        this.render()
+
+        return this.getGuess()
+
+
+      }}
+    
+    
+    render : function ()   { 
+        while (playerGuess >= 0 && playerGuess <= 100) {
+
+    if (playerGuess === this.secretNum) 
+    {
+      alert(`Congrats! You guessed the number in ${this.prevGuesses}!`)
+    } 
+      else if (playerGuess > this.secretNum) 
+      {
+        alert(`Your guess is too high Previous guesses: ${this.prevGuesses}`)
+      } 
+      else if (playerGuess < this.secretNum) 
+      {
+        alert(`Your guess is too low Previous guesses: ${this.prevGuesses}`)
+
+      }
+}
+}
+      
+
+  
   
 
   
-}
+
 
